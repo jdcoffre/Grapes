566919fe1663270c16fdb8fe8ffb58b906e63629
jdcoffre <jdcoffre@gmail.com>
2014-10-07 12:11:55 +0200

    Remove useless code

--- acceptance/src/test/java/org/axway/grapes/tests/acceptance/JBehaveTestCase.java
+++
@@ -1,7 +0,0 @@
-package org.axway.grapes.tests.acceptance;
-
-import net.thucydides.jbehave.ThucydidesJUnitStories;
-
-public class JBehaveTestCase extends ThucydidesJUnitStories {
-    public JBehaveTestCase() {}
-}
